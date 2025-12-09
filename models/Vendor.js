41df1ebee8935681c345973c99c87941b2021820
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-09 19:35:59 +0530

    Update Vendor.js

--- models/Vendor.js
+++ models/Vendor.js
@@ -4,20 +4,26 @@ const vendorSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
 
-    category: { type: String, required: true }, // "restaurant"
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true
+    },
 
     subcategories: [
       {
-        type: String, 
-        enum: ["veg", "non-veg", "biryani", "snacks", "drinks", "desserts"],
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubCategory"
       }
     ],
 
     type: { type: String },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
     email: String,
     phone: String,
     address: String,
+
     isApproved: { type: Boolean, default: false },
     metadata: { type: Object }
   },
